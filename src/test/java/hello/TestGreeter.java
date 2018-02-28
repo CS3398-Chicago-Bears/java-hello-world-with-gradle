cbbef9d4752d658033ab6bdc205b73a507e6f67c
MasonMilner <35643798+MasonMilner@users.noreply.github.com>
2018-02-28 17:43:18 -0600

    Update TestGreeter.java

--- src/test/java/hello/TestGreeter.java
+++ src/test/java/hello/TestGreeter.java
@@ -67,16 +67,16 @@ public class TestGreeter {
    {
 	g.setName("Omicron Persei 8");
 	   
-	assertGreaterThan(g.getName(), "Omicron Persei 7");
+	assertGreater(g.getName(), "Omicron Persei 7");
 	   
 	assertEquals(g.sayHello(),"Greetings puny Earthlings!");
    }
 	
-   public void newtestMPMGreeterFail()
-   {
-	g.setName("Rick King"); 
+  // public void newtestMPMGreeterFail()
+ //  {
+//	g.setName("Rick King"); 
 	   
-	assertNotEquals(g.getName(), "Rick King");   
-   }
+//	assertNotEquals(g.getName(), "Rick King");   
+ //  }
 	
 }
